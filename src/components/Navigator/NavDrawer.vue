d9389545ea87a943aed3a33ff290a0a1755c6c72
badjin <railrac23@gmail.com>
2020-06-22 10:32:23 +1000

    fix drawer scroll issue

--- src/components/Navigator/NavDrawer.vue
+++ src/components/Navigator/NavDrawer.vue
@@ -6,10 +6,12 @@
     temporary
     :expand-on-hover="expandOnHover"
     :mini-variant="miniVariant"
+    class="items"
   >
-    <v-img src="https://picsum.photos/1920/1080?random"
-           gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-           height="100%"
+    <v-img
+      src="https://picsum.photos/1920/1080?random"
+      gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+      height="100%"
     >
       <v-list dense nav class="py-0">
         <v-list-item
@@ -19,17 +21,18 @@
           link
           exact
         >
-          <v-img src="@/assets/logo.png"
-                 max-height="80"
-                 max-width="80"
-                 @click="goHome"
+          <v-img
+            src="@/assets/logo.png"
+            max-height="80"
+            max-width="80"
+            @click="goHome"
           />
           <v-list-item-content>
             Movie App
           </v-list-item-content>
         </v-list-item>
 
-        <v-divider class="mb-1"/>
+        <v-divider class="mb-1" />
 
         <v-list-item
           v-for="item in menuItems"
@@ -45,41 +48,46 @@
             {{ item.title }}
           </v-list-item-content>
         </v-list-item>
-
       </v-list>
     </v-img>
   </v-navigation-drawer>
 </template>
 
 <script>
+export default {
+  name: 'NavDrawer',
+  props: ['drawer', 'menuItems'],
+  data() {
+    return {
+      miniVariant: false,
+      expandOnHover: false,
+    };
+  },
 
-  export default {
-    name: "NavDrawer",
-    props: ['drawer', 'menuItems'],
-    data() {
-      return {
-        miniVariant: false,
-        expandOnHover: false
+  watch: {
+    drawer(val) {
+      if (!val) {
+        this.$emit('offDrawer');
       }
     },
+  },
 
-    watch: {
-      drawer(val) {
-        if (!val) {
-          this.$emit('offDrawer')
-        }
-      }
+  computed: {},
+  methods: {
+    goHome() {
+      this.$emit('goHome');
     },
-
-    computed: {
+    goTo(to) {
+      this.$emit('goTo', to);
     },
-    methods: {
-      goHome() {
-        this.$emit('goHome')
-      },
-      goTo(to) {
-        this.$emit('goTo', to)
-      },
-    }
-  }
+  },
+};
 </script>
+
+<style scoped>
+.items {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
